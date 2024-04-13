4c5581a12615ec0e1ae2baac52bec4458da8ea75
Vitaliy Murzin <murzinvit@yandex.ru>
2024-04-14 00:46:56 +0500

    add Test ParseCSVTest

--- src/test/java/ParseCSVTest.java
+++ src/test/java/ParseCSVTest.java
@@ -1,18 +1,21 @@
 import org.junit.jupiter.api.Assertions;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParseXMLTest {
+public class ParseCSVTest {
     @org.junit.jupiter.api.Test
     public void equalsOutput() {
         // given: Подготовительные данные
-        String testFileXml = "src/test/resources/data.xml";
+        String testFileCSV = "src/test/resources/data.csv";
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         List<Employee> expected = new ArrayList<Employee>();
-             expected.add(new Employee(1,"John","Smith","USA",25));
-             expected.add(new Employee(2,"Inav","Petrov","RU",23));
+
+        expected.add(new Employee(1,"John","Smith","USA",25));
+        expected.add(new Employee(2,"Inav","Petrov","RU",23));
 
         // when: Вызываемый целевой метод
-        List<Employee> actual = Main.parseXML(testFileXml);
+        List<Employee> actual = Main.parseCSV(columnMapping, testFileCSV);
 
         // then: Сравнение результата
         Assertions.assertEquals(expected.get(0).toString(), actual.get(0).toString());
